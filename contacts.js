ad74c5115287a8b0a005707d3add085f5a93824a
Oleh.Scherbak <oleh.scherbak@ukr.net>
2023-03-28 23:25:23 +0300

    initial

--- contacts.js
+++ contacts.js
@@ -4,22 +4,39 @@ const path = require("path");
 const contactsPath = path.resolve("db/contacts.json");
 
 async function listContacts() {
-  // ...твій код
-
   await fs
     .readFile(contactsPath)
     .then((data) => {
-      console.log(JSON.parse(data.toString()));
+      const result = JSON.parse(data.toString());
+      console.log(result);
     })
     .catch((err) => console.log("error: ", err.message));
 }
 
-function getContactById(contactId) {
-  // ...твій код
+async function getContactById(contactId) {
+  await fs
+    .readFile(contactsPath)
+    .then((data) => {
+      const contacts = JSON.parse(data.toString());
+      const contact = contacts.find((item) => item.id === contactId);
+      console.log(contact);
+    })
+    .catch((err) => console.log("error: ", err.message));
 }
 
-function removeContact(contactId) {
-  // ...твій код
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const newContactList = JSON.stringify(
+    await contacts.filter((contact) => contact.id !== contactId)
+  );
+
+  await fs
+    .writeFile(contactsPath, newContactList)
+    .then(() => {
+      console.log(`contact with id=${contactId} deleted successfully!`);
+      console.log(newContactList);
+    })
+    .catch((err) => console.log("error: ", err.message));
 }
 
 function addContact(name, email, phone) {
--- index.js
+++ index.js
@@ -1,3 +1,41 @@
 const contacts = require("./contacts");
+const { Command } = require("commander");
 
-contacts.listContacts();
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+console.log("argv:", argv);
+
+// TODO: рефакторить
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      contacts.listContacts();
+      break;
+
+    case "get":
+      contacts.getContactById(argv.id);
+      break;
+
+    case "add":
+      // ... name email phone
+      break;
+
+    case "remove":
+      contacts.removeContact(argv.id);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
